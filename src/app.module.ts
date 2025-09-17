3da47bb34111c853aa9da05ff11e75168b782346
Kevin Mokasana <kevin.mokasana@vinculumgroup.com>
2025-09-17 17:07:25 +0530

    typeorm config setup and entity added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { DatabaseConfig, ReadDatabaseConfig } from './orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv'
+dotenv.config()
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({...DatabaseConfig, name: process.env.WRITE_DB_NAME}),
+    TypeOrmModule.forRoot({...ReadDatabaseConfig, name: process.env.READ_DB_NAME}),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
